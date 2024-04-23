83a641957d1a7b9f26c2e81a7ca39ba12e805ca4
sujal21 <sujal1396.be21@chitkara.edu.in>
2024-04-23 23:24:14 +0530

    Made changes to particular user profiles

---
+++ frontend/src/dashboard/user-account/MyAccount.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import { authContext } from "./../../context/AuthContext";
+import userImg from "../../assets/images/doctor-img01.png";
+
+const MyAccount = () => {
+  const { dispatch } = useContext(authContext);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return (
+    <div className="max-w-[1170px] px-5 mx-auto">
+      <div className="grid md:grid-cols-3 gap-10">
+        <div className="pb-[50px] px-[30px] rounded-md">
+          <div className="flex items-center justify-center">
+            <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+              <img
+                src={userImg}
+                alt=""
+                className="w-full h-full rounded-full"
+              />
+            </figure>
+          </div>
+
+          <div className="text-center mt-4">
+            <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">
+              ABC
+            </h3>
+            <p className="text-textColor text-[15px] leading-6 font-medium">
+              user@gmail.com
+            </p>
+            <p className="text-textColor text-[15px] leading-6 font-medium">
+              Blood type:
+              <span className="ml-2 text-headingColor text-[22px] leading-8">
+                B+
+              </span>
+            </p>
+          </div>
+
+          <div className="mt-[50px] md:mt-[100px]">
+            <button
+              onClick={handleLogout}
+              className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+            >
+              Logout
+            </button>
+            <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+              Delete Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyAccount;
