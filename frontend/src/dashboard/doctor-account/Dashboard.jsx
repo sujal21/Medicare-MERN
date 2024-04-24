e2246010b1aef2e6fb2f6179556fc8d332102153
sujal21 <sujal1396.be21@chitkara.edu.in>
2024-04-24 18:55:39 +0530

    Updated doctor profile

--- frontend/src/dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/dashboard/doctor-account/Dashboard.jsx
@@ -4,6 +4,9 @@ import Error from "../../components/Error/Error";
 import useGetProfile from "../../hooks/useFetchData";
 import { BASE_URL } from "../../config";
 import Tabs from "./Tabs";
+import starIcon from "../../assets/images/Star.png";
+import DoctorAbout from "./../../pages/Doctors/DoctorAbout";
+import Profile from "./Profile";
 
 const Dashboard = () => {
   const { data, loading, error } = useGetProfile(
@@ -47,6 +50,56 @@ const Dashboard = () => {
                   </div>
                 </div>
               )}
+
+              <div className="mt-8">
+                {tab === "overview" && (
+                  <div>
+                    <div className="flex items-center gap-4 mb-10">
+                      <figure className="max-w-[200px] max-h-[200px]">
+                        <img src={data.photo} alt="" className="w-full" />
+                      </figure>
+
+                      <div>
+                        <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+                          {data.specialization} Surgeon
+                        </span>
+
+                        <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
+                          ABC
+                        </h3>
+
+                        <div className="flex items-center gap-[6px]">
+                          <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                            <img src={starIcon} alt="" />
+                            4.5
+                          </span>
+
+                          <span className="text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+                            (233)
+                          </span>
+                        </div>
+
+                        <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
+                          Doctor Bio
+                        </p>
+                      </div>
+                    </div>
+                    <DoctorAbout
+                      name={data.name}
+                      about={data.about}
+                      qualifications={data.qualification}
+                      experiences={data.experience}
+                    />
+                  </div>
+                )}
+
+                {tab === "appointments" && <div>appointments</div>}
+                {tab === "settings" && (
+                  <div>
+                    <Profile />
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         )}
--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { formateDate } from "../../components/utils/formatedate";
 
-const DoctorAbout = () => {
+const DoctorAbout = ({ name, about, qualifications, experiences }) => {
   return (
     <div>
       <div>
